3b926d75ba2e54a1993b9de20c2800d26b4eb443
yuzai-code <958684803@qq.com>
2024-11-05 18:22:49 +0800

    应用标题“ Tool”，移除PrimeVue相关依赖，添加Naive UI，并相应更新package.json和pnpm-lock.yaml中的依赖项。同时，删除了与PrimeVue主题相关的代码

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,13 +1,5 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import PrimeVue from "primevue/config";
-import Aura from "@primevue/themes/aura";
 
 createApp(App).mount("#app");
 
-const app = createApp(App);
-app.use(PrimeVue, {
-  theme: {
-    preset: Aura,
-  },
-});
