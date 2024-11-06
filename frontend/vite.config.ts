93666a3baf2d1a9e7b156e1b989fc388a9b952e0
yuzai-code <958684803@qq.com>
2024-11-06 18:26:57 +0800

    feat: 更新 App.vue 组件，新增 sidebar 组件；移除模板式，简化配置并调整服务器端口和热重载相关选项，添加针对 Tauri 的构建目标选项；新建 sidebar.vue

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,39 +1,26 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import { loadEnv } from 'vite';
+import { defineConfig } from "vite";
 
-// 从环境变量中获取 Tauri 开发主机地址，如果没有设置则默认为 'localhost'
-const host = process.env.TAURI_DEV_HOST || 'localhost';
+const host = "localhost";
 
-export default defineConfig(async ({ command, mode }) => {
-  // 根据当前模式加载环境变量
-  const env = loadEnv(mode, process.cwd(), '');
-
-  return {
-    plugins: [vue()],
-
-    // 针对 Tauri 开发的 Vite 选项，只在 `tauri dev` 或 `tauri build` 时应用
-    clearScreen: false, // 防止 Vite 遮蔽 Rust 错误信息
-    server: {
-      port: 1420, // Tauri 期望一个固定的端口，如果该端口不可用则失败
-      strictPort: true, // 确保端口号严格匹配
-      host: host, // 使用环境变量中的主机地址，如果没有则使用 'localhost'
-      hmr: host
-        ? {
-            protocol: 'ws',
-            host,
-            port: 1421, // 热更新端口号
-          }
-        : undefined, // 如果不在 Tauri 开发主机模式下，则不启用 HMR
-      watch: {
-        // 忽略 `src-tauri` 目录的文件变动，因为 Tauri 会处理这部分
-        ignored: ['**/src-tauri/**'],
-      },
-    },
-
-    // 定义全局变量 __APP_ENV__，用于在应用中访问环境变量
-    define: {
-      __APP_ENV__: JSON.stringify(env.APP_ENV),
-    },
-  };
+export default defineConfig({
+  // 防止 Vite 清除 Rust 显示的错误
+  clearScreen: false,
+  server: {
+    // Tauri 工作于固定端口，如果端口不可用则报错
+    strictPort: true,
+    // 如果设置了 host，Tauri 则会使用
+    host: host || false,
+    port: 5173,
+  },
+  // 添加有关当前构建目标的额外前缀，使这些 CLI 设置的 Tauri 环境变量可以在客户端代码中访问
+  envPrefix: ["VITE_", "TAURI_ENV_*"],
+  build: {
+    // Tauri 在 Windows 上使用 Chromium，在 macOS 和 Linux 上使用 WebKit
+    target:
+      process.env.TAURI_ENV_PLATFORM == "windows" ? "chrome105" : "safari13",
+    // 在 debug 构建中不使用 minify
+    minify: !process.env.TAURI_ENV_DEBUG ? "esbuild" : false,
+    // 在 debug 构建中生成 sourcemap
+    sourcemap: !!process.env.TAURI_ENV_DEBUG,
+  },
 });
